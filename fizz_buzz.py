0607fc23b4f2cfe4cd9daacfeb3b022e6c5ea38f
serfer2 <serfer2@gmail.com>
2020-02-27 20:27:45 +0100

    Implements fizzbuzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -2,4 +2,21 @@
 
 
 def fizz_buzz(value):
+    # Comprobamos el tipo de la variable
+    tipo_de_la_variable = type(value)  # int, float, str, dict, list, etc...
+
+    if tipo_de_la_variable not in (int, float):
+        raise TypeError
+
+    # Comprobamos que sea divisible por 5 y por 3
+    if value % 3 == 0 and value % 5 == 0:
+        return 'fizz buzz'
+    # Comprobamos que sea divisible por 3
+    if value % 3 == 0:
+        return 'fizz'
+    # Comprobamos que sea divisible por 5
+    if value % 5 == 0:
+        return 'buzz'
+
     return value
+
--- test/test_fizz_buzz.py
+++ test/test_fizz_buzz.py
@@ -12,8 +12,36 @@ class TestFizzBuzz(unittest.TestCase):
             fizz_buzz(value='a string')
 
     def test_it_returns_same_number_as_received(self):
+        # Acción
         output = fizz_buzz(value=1)
+        # Comprobación
         self.assertEqual(output, 1)
+
+    def test_it_returns_fizz_when_value_is_divisible_by_3(self):
+        # Inicializo
+        a_number_divisible_by_3 = 9
+        expected_result = 'fizz'
+        # Accion
+        output = fizz_buzz(value=a_number_divisible_by_3)
+
+        # Comprobación
+        self.assertEqual(output, expected_result)
+
+    def test_it_returns_buzz_when_value_is_divisible_by_5(self):
+        a_number_divisible_by_5 = 10
+        expected_result = 'buzz'
+
+        output = fizz_buzz(value=a_number_divisible_by_5)
+
+        self.assertEqual(output, expected_result)
+
+    def test_it_returns_fizzbuzz_when_value_is_divisible_by_5_and_3(self):
+        a_number_divisible_by_5_and_3 = 15
+        expected_result = 'fizz buzz'
+
+        output = fizz_buzz(value=a_number_divisible_by_5_and_3)
+
+        self.assertEqual(output, expected_result)
 
 
 if __name__ == '__main__':
